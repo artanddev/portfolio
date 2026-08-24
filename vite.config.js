d9ccc4dc99fb1694f924e1628fc3bcfccba90af3
linhuuuu <linhuuss@gmail.com>
2026-08-24 20:37:09 +0800

    redo: app creation
    deprecated template

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import react from '@vitejs/plugin-react'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/artanddev-portfolio/',
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
+  ],
+  base : ['/portfolio/'],
+
 })
